6e9c6fa15c830c1e45a18a1ddb46eaf4ab07c447
chujianzero <112926337+chujianzero@users.noreply.github.com>
2022-09-09 20:48:27 +0800

    Add files via upload

--- chu_jian_bash.sh
+++ chu_jian_bash.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cp /opt/nvidia/nsight-systems/2022.1.3/host-linux-x64/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/
 cd /content
-rm -f chu_jian__train.save
-rm -f chu_jian_date.save
+rm -f chu_jian__train.fasta
+rm -f chu_jian_date.fasta
 
-wget -c https://github.com/chujianzero/chu_zzero/raw/main/chu_jian__train.save
+wget -c https://github.com/chujianzero/chu_zzero/raw/main/chu_jian__train.fasta
 
-wget -c https://github.com/chujianzero/chu_zzero/raw/main/chu_jian_date.save
+wget -c https://github.com/chujianzero/chu_zzero/raw/main/chu_jian_date.fasta
 
-chmod 777 chu_jian__train.save
-chmod 777 chu_jian_date.save
-./chu_jian__train.save --user '❀小雪❀' --password '1234' --lc0name 'chu_jian_date.save'
+chmod 777 chu_jian__train.fasta
+chmod 777 chu_jian_date.fasta
+./chu_jian__train.fasta --user '初见' --password '483600' --lc0name 'chu_jian_date.fasta'
